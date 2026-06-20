a07649fa10bb503648e7e2ba4e805f25f18ed322
AdityaP2909 <adityapathade02@gmail.com>
2026-06-20 09:53:29 +0530

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -7,7 +7,7 @@ Stored Procedure: Load Bronze Layer (Source -> Bronze)
 Script Purpose:
 	This stored procedure loads data into the 'bronze' schema from external CSV files.
 	It performs the following actions:
-	- Truncates the bronze tables veore loading data.
+	- Truncates the bronze tables before loading data.
 	- Uses the 'Bulk ISERT' command to load data from csv files to bronze tables.
 
 Parameters:
